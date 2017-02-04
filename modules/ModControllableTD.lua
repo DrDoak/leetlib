96329e69ec472f028f9d292a34dca79f1b69a80a
LuoLei_Zhao <LuoLei_Zhao>
2017-02-03 21:38:21 -0600

    Removed dependency of TD modules on non-TD modules

--- modules/ModControllableTD.lua
+++ modules/ModControllableTD.lua
@@ -1,9 +1,8 @@
-local ModControllable = require "modules.ModControllable"
 local Keymap  = require "xl.Keymap"
-local ObjIntHitbox = require "objects.ObjIntHitboxTD"
+local ObjIntHitbox = require "objects.ObjIntHitbox"
 
-local ModControllableTD = Class.create("ModControllableTD", ModControllable)
-ModControllableTD.dependencies = {"ModActive","ModInventory"}
+local ModControllableTD = Class.create("ModControllableTD", Entity)
+ModControllableTD.dependencies = {"ModActive","ModInventory","ModCharacter","ModCharacterDetector"}
 ModControllableTD.trackFunctions = {"normalState"}
 
 
@@ -13,7 +12,8 @@ function ModControllableTD:normalState()
 	self:animate()
 	self:proccessInventory()
  	xl.DScreen.print("charpos: ", "(%f,%f)",self.x,self.y)
-
+ 	xl.DScreen.print("charDir: ", "(%f)",self.dir)
+ 	xl.DScreen.print("speedMod: ", "(%f,%f)",self.speedModX,self.speedModY)
 end
 
 --Manages left/right
@@ -26,28 +26,30 @@ function ModControllableTD:normalMove(maxSpeedX, maxSpeedY)
 	local dvX,xdir,dvY,ydir = 0,0,0,0
 	if Keymap.isDown("up") then 
 		dvY = dvY - 1
-		self.dir = -1 --0
+		self.dir = 0
 	end
 	if Keymap.isDown("down") then 
 		dvY = dvY + 1
-	 	self.dir = 1 --2
+	 	self.dir = 2
 	end
 
 	if Keymap.isDown("left") then 
 		dvX = dvX - 1
 		self.dir = -1 
+		-- self.body:setLinearVelocity(-32*3,0)
 	end
 	if Keymap.isDown("right") then 
 		dvX = dvX + 1
 	 	self.dir =   1 
+		-- self.body:setLinearVelocity(32*3,0)
 	end
-	if dvX ~= 0 and math.abs(self.velX - self.referenceVelX) < maxSpeedX * self.speedModX then
+	if dvX ~= 0 and math.abs(self.velX - self.referenceVelX) < (maxSpeedX * self.speedModX) then
 		self.forceX = dvX * accForce
 		if util.sign(self.velX) == dvX then
 			self.forceX = self.forceX * 2
 		end
 	end
-	if dvX ~= 0 and math.abs(self.velX - self.referenceVelY) < maxSpeedY * self.speedModY then
+	if dvY ~= 0 and math.abs(self.velY - self.referenceVelY) < (maxSpeedY * self.speedModY) then
 		self.forceY = dvY * accForce
 		if util.sign(self.velY) == dvY then
 			self.forceY = self.forceY * 2
@@ -59,10 +61,81 @@ function ModControllableTD:normalMove(maxSpeedX, maxSpeedY)
 	self.isMovingY = (dvY ~= 0) or self.inAir
 end
 
-function ModControllable:proccessInventory()
-	if Keymap.isPressed("interact") and not Game.DialogActive then
-		local intHitbox = ObjIntHitboxTD(self) 
-		Game:add(intHitbox)
+function ModControllableTD:lockOnControls( )
+	if Keymap.isPressed("lockon") then
+		local targets = Game:findObjectsWithModule("ModCharacter")
+		local scorePriority 
+		-- local scores = {}
+		local minScore = 9999999
+		local minObj = nil
+
+		for i,obj in ipairs(targets) do
+			-- lume.trace(obj.type)
+			if self:validTarget(obj) then
+				local dist = self:getDistanceToPoint(obj.x,obj.y)
+				local modDir = self.dir
+				if Keymap.isDown("left") then
+					modDir = -1
+				elseif Keymap.isDown("right") then
+					modDir = 1
+				elseif Keymap.isDown("up") then
+					modDir = 0
+				elseif Keymap.isDown("down") then
+					modDir = 2
+				end
+				local angOffset = self:offsetFromView(obj,modDir)
+				local score = dist* (math.max(math.pi/6,angOffset))
+				if score < minScore then
+					minScore = score
+					minObj = obj
+				end
+			end
+		end
+		if minObj then
+			self:setTarget(minObj)
+		end
 	end
 end
+function ModControllableTD:proccessInventory()
+	self:lockOnControls()
+
+	if Keymap.isPressed("interact") then
+		if self.targetObj then
+			self:setTarget(nil)
+		elseif not Game.DialogActive then
+			local intHitbox = ObjIntHitbox(self) 
+			Game:add(intHitbox)
+		end
+	end
+	if Keymap.isPressed("use") then
+		-- lume.trace()
+		if self.currentEquips["neutral"] then
+			-- lume.trace()
+			self.currentEquips["neutral"]:use()
+		end
+	end
+end
+
+function ModControllableTD:performResponse( eventName, eventTags, params )
+	self.proposedActions = {}
+	for tagName,tagValue in pairs(eventTags) do
+		self:checkIfNeedNewTag(tagName)
+		for topic,allFunctions in pairs(self.allTagResponses[tagName]) do
+			for i,realFunction in ipairs(allFunctions) do
+				-- lume.trace(topic.name,tagName,realFunction)
+				realFunction(self,params,tagName,eventTags,topic) --,self.AIPieces[aiPieceName])
+			end
+		end
+	end
+	-- self:executeProposedActions()
+end
+
+-- function ModControllableTD:proposeAction()
+-- 	-- body
+-- end
+
+-- function ModControllableTD:executeProposedActions()
+-- 	-- body
+-- end
+
 return ModControllableTD
--- modules/ModDrawableTD.lua
+++ modules/ModDrawableTD.lua
@@ -1,9 +1,11 @@
-local ModDrawable = require "modules.ModDrawable"
-local ModDrawableTD = Class.create("ModDrawableTD", ModDrawable)
+local ModDrawableTD = Class.create("ModDrawableTD", Entity)
 
-function ModDrawable:create()
+ModDrawableTD.trackFunctions = {"draw"}
+
+function ModDrawableTD:create()
 	self.animations = self.animations or {}
 	self.sprites = self.sprites or {}
+	self.spritePieceNames = self.spritePieceNames or {}
 	self.idleCounter = 0
 	-- self.angle = 0
 	self.imgX = 64
@@ -11,95 +13,29 @@ function ModDrawable:create()
 	self.initImgH = 64
 	self.initImgW = 64
 
-	self.lastDir = 0
+	self.x = self.x or 0
+	self.y = self.y or 0
+	self.height = self.height or 0
+
+
+	-- self.lastDir = 0
+	self.referenceVel = 0
 	self.animationsPending = {}
 	self.attachPositions = {}
 	self.icons = {}
 end
 
-function ModDrawableTD:updateSprites()
-	if not self.angle and (self.body and not self.body:isFixedRotation()) then
-		self:setSprAngle(self.body:getAngle())
-	end
-	for key,value in pairs(self.sprites) do
-		if self.sprites[key].noLoop == false then
-			self:changeAnimation(self.sprites[key].currentAnim )
-		end
-		Game.scene:move(value,self.y)
-	end
-	self:setSprPos(self.x,self.y + 29 + self.height/2)
+function ModDrawableTD:tick(dt)
+	self:updateSprites() 
 end
 
-function ModDrawableTD:setSprPos( x , y )
-	for key, piece in pairs(self.sprites) do
-		local piecesPos = piece:updatePos(x,y - piece.vert/2)
-		for k,v in pairs(piecesPos) do
-			self.attachPositions[k] = v
-		end
+function ModDrawableTD:destroy()
+	for key, value in pairs(self.sprites) do
+		self:delSpritePiece(key)
 	end
 	if self.sprite then
-		self.sprite:setPosition(x,y)
-	end
-end
-
-function ModDrawableTD:animate()
-	local maxSpeed, maxSpeedY = self.maxSpeed, self.maxSpeedY
-	local walkanim = math.abs(4 / self.velX)
-	local newVelX = self.velX - self.referenceVelX
-	local newVelY = (self.velY - self.referenceVelY) * 1.4
-	local newVel = math.sqrt(math.pow(newVelX,2) + math.pow(newVelY,2))
-
-	if self.isMoving then
-		self.idleCounter = 0
-		if (self.dir == 1 and newVelX < -16) or (self.dir == -1 and newVelX > 16) then
-			self:changeAnimation({"slideMore","slide","stand"})
-		else
-			if math.abs(newVel) >= maxSpeed - 52 then
-				self:changeAnimation({"run","walk"})
-			else
-				self:changeAnimation("walk")
-			end
-		end
-	else
-		if math.abs(newVelX) <= 32 then
-			self.idleCounter = self.idleCounter + 1
-			if self.idleCounter >= 60 and self.idleCounter < 89 then
-				self:changeAnimation({"idleStart","idle","stand"})
-			elseif self.idleCounter > 84 then
-				self:changeAnimation({"idle","stand"})
-			else
-				self:changeAnimation("stand")
-			end
-		else
-			self:changeAnimation({"slide","stand"})
-		end
-	end
-	if self.isHolding then
-		self:changeAnimation({"holding","guard"})
-	end
-end
-
-function ModDrawableTD:orientAllSprites()
-	for key,value in pairs(self.sprites) do
-		if self.dir == -1 or self.dir == 1 then
-			value:setScale(self.dir * value.mDir,1)
-		else
-			value:setScale(value.mDir,1)
-		end
-		value.dir = self.dir
-	end
-end
-function ModDrawableTD:orientSprite(row,range,delay,startFrame, sprite, onLoop)
-	local spr = self.sprites[sprite] or self.sprite
-	if delay == 0 then delay = 0.1 end
-	local md = self.sprites[sprite].mDir
-	if self.dir == -1 or self.dir == 1 then
-		spr:setScale(self.dir * md,1)
-	else
-		spr:setScale(md,1)
-	end
-	spr:setAnimation(range,row,1/delay,onLoop)
-	spr.dir = self.dir * md
+		Game.scene:remove(self.sprite)
+	end 
 end
 
 function ModDrawableTD:changeAnimation(animation,speedMod,spritePieces)
@@ -137,20 +73,29 @@ function ModDrawableTD:changeAnimation(animation,speedMod,spritePieces)
 					if self.dir == 0 then
 						range = anim2.upRange or range
 						row = anim2.upRow or row
+						delay = anim2.upDelay or delay * 2
 					elseif self.dir == 2 then
 						range = anim2.downRange or range
+						delay = anim2.downDelay or delay * 2
 						row = anim2.downRow or row
 					end
 					local priority = (anim2.priority or 1)
-					if (#self.animationsPending == 0 and priority >= self.sprites[key].priority) then
-						self.sprites[key]:updateAttach(value.attachMod or {{{x=0,y=0}}})
-						if goodKey ~= self.sprites[key].currentAnim then
-							--self.sprites[key].attachF = #self.sprites[key].attachMod
+					if true then --(#self.animationsPending == 0 and priority >= self.sprites[key].priority) then
+						if self.dir == 0 then
+							self.sprites[key]:updateAttach(value.attachUp or value.attachMod or {{{x=0,y=0}}})
+						elseif self.dir == 2 then
+							self.sprites[key]:updateAttach(value.attachDown or value.attachMod or {{{x=0,y=0}}})
+						else
+							self.sprites[key]:updateAttach(value.attachMod or {{{x=0,y=0}}})
+						end
+						--self.sprites[key]:updateAttach(value.attachDown)
+						if goodKey ~= self.sprites[key].currentAnim or self.sprites[key].currentDir ~= self.dir then
 							self.sprites[key]:setIndex(0)
 							self.sprites[key]:onUpdate()
 							self.sprites[key]:resume()
 						end
 						self.sprites[key].mDir = mDir
+						self.sprites[key].currentDir = self.dir
 						self.sprites[key].currentAnim = goodKey
 						self.sprites[key].priority = priority
 						self:orientSprite(row,range,delay,startFrame, key,noLoop)
@@ -163,12 +108,155 @@ function ModDrawableTD:changeAnimation(animation,speedMod,spritePieces)
 	return hasAnimation
 end
 
+
+function ModDrawableTD:updateSprites()
+	if self.body then
+		self.x,self.y = self.body:getPosition()
+	end
+	if not self.angle and (self.body and not self.body:isFixedRotation()) then
+		self:setSprAngle(self.body:getAngle())
+	end
+	for i,spriteName in ipairs(self.spritePieceNames) do
+		local spr = self.sprites[spriteName]
+		if spr.noLoop == false then
+			self:changeAnimation(spr.currentAnim )
+		end
+		Game.scene:move(spr,self.y + (spr.zDiff or 0))
+	end
+	self:setSprPos(self.x,self.y + 29 + math.floor(self.height/2))
+end
+
+function ModDrawableTD:orientAllSprites()
+	for key,value in pairs(self.sprites) do
+		if self.dir == -1 or self.dir == 1 then
+			value:setScale(self.dir * value.mDir,1)
+		else
+			value:setScale(value.mDir,1)
+		end
+		value.dir = self.dir
+	end
+end
+function ModDrawableTD:orientSprite(row,range,delay,startFrame, sprite, onLoop)
+	local spr = self.sprites[sprite] or self.sprite
+	if delay == 0 then delay = 0.1 end
+	local md = self.sprites[sprite].mDir
+	if self.dir == -1 or self.dir == 1 then
+		spr:setScale(self.dir * md,1)
+	else
+		spr:setScale(md,1)
+	end
+	spr:setAnimation(range,row,1/delay,onLoop)
+	spr.dir = self.dir * md
+end
+
+function ModDrawableTD:freezeAnimation(sprite, duration)
+	if self.sprites[sprite] then
+		self.sprites[sprite]:pause(duration)
+	end
+end
+
+function ModDrawableTD:resetAnimation(spritePiece)
+	if self.sprites[spritePiece] then
+		self.sprites[spritePiece]:resetAnimation()
+		self.sprites[spritePiece]:onUpdate()
+	end
+end
+function ModDrawableTD:overrideAnimation(spritePiece)
+	if self.sprites[spritePiece] then
+		self.sprites[spritePiece].priority = 0
+	end
+end
+
+function ModDrawableTD:normalizeSprSize( speed )
+	local s = speed or 8
+	self.imgX = math.min( self.imgX + s, self.initImgW )
+	self.imgY = math.min( self.imgY + s, self.initImgH)
+end
+
+-- function ModDrawableTD:animate()
+-- 	-- lume.trace()
+-- 	local maxSpeed, maxSpeedY = self.maxSpeed, self.maxSpeedY
+-- 	local walkanim = math.abs(4 / self.velX)
+-- 	local newVelX = self.velX - self.referenceVel
+-- 	walkanim = math.max(walkanim, 0.18)
+
+-- 	if self.inAir then 
+-- 		if self.turnTime and self.turnTime > 0 then
+-- 				self:changeAnimation({"fallTurn","fall"})
+-- 		elseif not self.turnTime and self.velY < 0 or self.jumping then
+-- 			self:changeAnimation("jump")
+-- 		else
+-- 			self:changeAnimation({"fall","jump"})
+-- 		end
+-- 	elseif self.isCrouching then
+-- 		self:changeAnimation({"crouch","stand"})
+-- 	elseif self.isMoving then
+-- 		self.idleCounter = 0
+-- 		if (self.dir == 1 and newVelX < -16) or (self.dir == -1 and newVelX > 16) then
+-- 			self:changeAnimation({"slideMore","slide","stand"})
+-- 		else
+-- 			if self.status == "offense" and self.prepTime > 5 then
+-- 				--self:changeAnimation("prep2")
+				
+-- 				--self:freezeAnimation("body",0.0)
+-- 				--self:freezeAnimation("head",0.0)
+-- 			end 
+-- 			if math.abs(newVelX) >= maxSpeed - 52 then
+-- 				self:changeAnimation({"run","walk"})
+-- 			else
+-- 				self:changeAnimation("walk")
+-- 			end
+-- 		end
+-- 	else
+-- 		if math.abs(newVelX) <= 32 then
+-- 			self.idleCounter = self.idleCounter + 1
+-- 			if self.status == "offense" and self.prepTime > 5 then
+-- 			--	self:changeAnimation({"prep","stand"})
+-- 			else
+-- 				if self.idleCounter >= 60 and self.idleCounter < 89 then
+-- 					self:changeAnimation({"idleStart","idle","stand"})
+-- 				elseif self.idleCounter > 84 then
+-- 					self:changeAnimation({"idle","stand"})
+-- 				else
+-- 					self:changeAnimation("stand")
+-- 				end
+-- 			end
+-- 			-- if self.idleCounter >= 84 then
+-- 			-- 	self.idleCounter = 0
+-- 			-- end
+-- 		else
+-- 			self:changeAnimation({"slide","stand"})
+-- 		end
+-- 	end
+-- 	if self.isHolding then
+-- 		self:changeAnimation({"holding","guard"})
+-- 	end
+-- end
+
+function ModDrawableTD:addSpritePieces( newPieces )
+	local depth = self.depth
+	for key, piece in pairs(newPieces) do
+		self:addSpritePiece(piece ,depth)
+		depth = depth + 1
+	end
+end
+
+function ModDrawableTD:addSprite( piece )
+	self.sprite = xl.Sprite(piece.path, piece.width, piece.height)
+	self.sprite:setSize((piece.imgX or piece.width/2), (piece.imgY or piece.height/2))
+	self.sprite:setOrigin((piece.originX or piece.width/2), (piece.originY or piece.height/2))
+	self:changeAnimation(1,1,0, 1)
+	Game.scene:insert(self.sprite)
+end
+
 function ModDrawableTD:addSpritePiece( piece , d)
 	local sprite
 	local SpritePiece = require "xl.SpritePiece"
 	self.advancedSprites = true
-
-	sprite = SpritePiece(piece.path, (piece.width or 128), (piece.height or 128),0,d)
+	if not util.hasValue(self.spritePieceNames,piece) then
+		table.insert(self.spritePieceNames,piece.name)
+	end
+	sprite = SpritePiece(piece.path, (piece.width or 128), (piece.height or 128),0,d,piece.name)
 	local imgY = (piece.imgY or piece.height/2)
 	sprite:setOrigin((piece.originX or piece.width/2), (piece.originY or piece.height/2))
 	sprite:setSize((piece.imgX or piece.width/2), imgY)
@@ -187,10 +275,14 @@ function ModDrawableTD:addSpritePiece( piece , d)
 				break
 			end
 		end
+		if connectSprite == nil then
+			lume.trace('mType: ',self.type, "currentSprite", piece.name,"connectSprite:",piece.connectSprite)
+			error('Attempted to connect Sprite to spritePiece that does not exist')
+		end
 		sprite:addConnectPoint(connectSprite, piece.connectPoint,piece.connectMPoint)
 	end
 	sprite:setAnimation(1,1,1)
-	--ssprite:setDepth(d + (piece.z or 0))
+	-- sprite:setDepth(d + (piece.z or 0))
 	Game.scene:insert(sprite)
 	self.sprites[piece.name] = sprite
 	if piece.animations then
@@ -207,6 +299,114 @@ function ModDrawableTD:addSpritePiece( piece , d)
 			end
 			self.animations[k]["sprites"][piece.name] = v
 		end
+	end
+end
+
+
+function ModDrawableTD:delSpritePiece( pieceName )
+	if self.sprites[pieceName] then
+		Game.scene:remove(self.sprites[pieceName])
+		self.sprites[pieceName] = nil
+	end
+end
+
+function ModDrawableTD:getAttachPos(attachPoint )
+	return self.attachPositions[attachPoint]
+end
+
+function ModDrawableTD:setSprPos( x , y )
+	self.lastY = y
+	for i, spriteName in ipairs(self.spritePieceNames) do
+		local piece = self.sprites[spriteName]
+		local piecesPos = piece:updatePos(x,y - piece.vert/2)
+		for k,v in pairs(piecesPos) do
+			self.attachPositions[k] = v
+		end
+	end
+	if self.sprite then
+		self.sprite:setPosition(x,y)
+	end
+end
+
+
+function ModDrawableTD:setSprAngle( angle )
+	for key, piece in pairs(self.sprites) do
+		piece:setAngle(angle)
+	end
+	if self.sprite then
+		self.sprite:setAngle(angle)
+	end
+end
+
+function ModDrawableTD:setDepth( depth )
+	self.depth = depth
+end
+
+
+function ModDrawableTD:addIcon( newIcon )
+	local newPieces
+	local newTable = {}
+	newIcon = util.deepcopy(newIcon)
+	for i,v in ipairs(self.icons) do
+		if v.path == newIcon.path then
+			return 
+		end
+	end
+	local iconName = "icon" .. #self.icons + 1
+	newIcon.name = iconName
+	if #self.icons > 0 then
+		newIcon.connectSprite = "icon" .. (#self.icons)
+	elseif self.sprites["main"] then
+		newIcon.connectSprite = "main"
+		newIcon.connectPoint = "center"
+		newIcon.attachPoints.prevIco = {x=16,y=48}
+	elseif self.sprites["legs"] then
+		newIcon.connectSprite = "legs"
+		newIcon.connectPoint = "center"
+		newIcon.attachPoints.prevIco = {x=16,y=48}
+	end
+	-- lume.trace(newIcon.path)
+	-- lume.trace(iconName)
+	-- util.print_table(self.icons)
+	-- lume.trace(Game:getTicks())
+
+	self:addSpritePiece(newIcon)
+	-- lume.trace(self.sprites[newIcon.name].setAngle)
+	self.icons[#self.icons + 1] = newIcon
+end
+
+function ModDrawableTD:removeIcon( iconPath )
+	local pushBack = false
+	local deletedInd = 0
+	for i,v in pairs(self.icons) do
+		if v.path == iconPath then
+			pushBack = true
+			self:delSpritePiece(v.name)
+			deletedInd = i
+			-- lume.trace("I is: ",i,"removed: ", v.name, "path: ",iconPath)
+		elseif pushBack then
+			self:delSpritePiece(v.name)
+			v.connectSprite = "icon" .. (i - 2)
+			v.name = "icon" .. (i - 1)
+			if (i-1) == 1 then
+				if self.sprites["main"] then
+					v.connectSprite = "main"
+				else
+					v.connectSprite = "legs"
+				end
+				v.connectPoint = "center"
+				v.attachPoints.prevIco = {x=16,y=48}
+			end
+			lume.trace("prev was: ", i, "Now is: " , v.name)
+			lume.trace("trying to connect to: " , v.connectSprite)
+			self:addSpritePiece(v)
+		end
+	end
+
+	if deletedInd ~= 0 then
+		table.remove(self.icons,deletedInd)
+	else
+		lume.trace("Attempting to remove icon that does not exist.")
 	end
 end
 return ModDrawableTD
